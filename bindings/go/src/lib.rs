32731dd86627c5e47cb51819b51ffd2d17a1a91c
arsalan-anwari <arsalan@anwari.nl>
2026-05-26 17:42:36 +0200

    fixed ci/cd errors, added local test_ci script.

--- bindings/go/src/lib.rs
+++ bindings/go/src/lib.rs
@@ -2,5 +2,5 @@ use std::os::raw::c_char;
 
 #[no_mangle]
 pub extern "C" fn zlicenser_hello_world() -> *const c_char {
-    b"hello from zlicenser\0".as_ptr() as *const c_char
+    c"hello from zlicenser".as_ptr()
 }
