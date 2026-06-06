02c3c5188082b023c31ee0ea77a67d1259d92526
arsalan-anwari <arsalan@anwari.nl>
2026-06-06 22:11:38 +0200

    standardtize deploy script and fix deny errors in ci

--- publish.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-if [ -z "${ZER_CRATES_IO_TOKEN:-}" ]; then
-  echo "Error: ZER_CRATES_IO_TOKEN is not set." >&2
-  exit 1
-fi
-
-cargo publish --token "$ZER_CRATES_IO_TOKEN"
